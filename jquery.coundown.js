518193bdf31d6b348e25b8ccd8e97537eea4be16
creativethings11 <90059147+creativethings11@users.noreply.github.com>
2026-03-24 21:11:54 +0300

    Update jquery.coundown.js

--- jquery.coundown.js
+++ jquery.coundown.js
@@ -1,112 +1,88 @@
+// jscountdown.js
 // CountDown Clock
-// Version   : 1.0.1
-// Developer : Ekrem KAYA
+// Version   : 1.0.4 (English labels)
+// Developer : Ekrem KAYA, modified for live countdown
 // Website   : https://e-piksel.com
 // GitHub    : https://github.com/epiksel/countdown
 
 (function ($) {
-	$.fn.countdown = function (options, callback) {
-		var settings = $.extend({
-			date: null,
-			offset: null,
-			day: 'Hari',
-			days: 'Hari',
-			hour: 'Jam',
-			hours: 'Jam',
-			minute: 'Menit',
-			minutes: 'Menit',
-			second: 'Detik',
-			seconds: 'Detik'
-		}, options);
+  $.fn.countdown = function (options, callback) {
+    var settings = $.extend({
+      date: null,       // Target date for countdown
+      offset: 0,        // Timezone offset in hours (e.g., 5.5 for IST)
+      day: 'Day',
+      days: 'Days',
+      hour: 'Hour',
+      hours: 'Hours',
+      minute: 'Minute',
+      minutes: 'Minutes',
+      second: 'Second',
+      seconds: 'Seconds'
+    }, options);
 
-		// Throw error if date is not set
-		if (!settings.date) {
-			$.error('Date is not defined.');
-		}
+    if (!settings.date) {
+      $.error('Date is not defined.');
+    }
 
-		// Throw error if date is set incorectly
-		if (!Date.parse(settings.date)) {
-			$.error('Incorrect date format, it should look like this, 12/24/2012 12:00:00.');
-		}
+    if (!Date.parse(settings.date)) {
+      $.error('Incorrect date format, it should look like this: 12/24/2012 12:00:00');
+    }
 
-		// Save container
-		var container = this;
+    var container = this;
 
-		/**
-		 * Change client's local date to match offset timezone
-		 * @return {Object} Fixed Date object.
-		 */
-		var currentDate = function () {
-			// get client's current date
-			var date = new Date();
+    // Get client's current date with timezone offset
+    function currentDate() {
+      var date = new Date();
+      var utc = date.getTime() + (date.getTimezoneOffset() * 60000);
+      return new Date(utc + (3600000 * settings.offset));
+    }
 
-			// turn date to utc
-			var utc = date.getTime() + (date.getTimezoneOffset() * 60000);
+    // Main countdown function
+    function countdown() {
+      var target_date = new Date(settings.date);
+      var now = currentDate();
+      var difference = target_date - now;
 
-			// set new Date object
-			var new_date = new Date(utc + (3600000*settings.offset));
+      if (difference < 0) {
+        clearInterval(interval);
+        if (callback && typeof callback === 'function') callback();
+        container.html("<li><p>Event Started!</p></li>");
+        return;
+      }
 
-			return new_date;
-		};
+      var _second = 1000,
+          _minute = _second * 60,
+          _hour = _minute * 60,
+          _day = _hour * 24;
 
-		/**
-		 * Main countdown function that calculates everything
-		 */
-		function countdown () {
-			var target_date = new Date(settings.date), // set target date
-				current_date = currentDate(); // get fixed current date
+      var days = Math.floor(difference / _day),
+          hours = Math.floor((difference % _day) / _hour),
+          minutes = Math.floor((difference % _hour) / _minute),
+          seconds = Math.floor((difference % _minute) / _second);
 
-			// difference of dates
-			var difference = target_date - current_date;
+      // Format numbers to two digits
+      days = days < 10 ? '0' + days : days;
+      hours = hours < 10 ? '0' + hours : hours;
+      minutes = minutes < 10 ? '0' + minutes : minutes;
+      seconds = seconds < 10 ? '0' + seconds : seconds;
 
-			// if difference is negative than it's pass the target date
-			if (difference < 0) {
-				// stop timer
-				clearInterval(interval);
+      // Update DOM
+      container.find('.days').text(days);
+      container.find('.hours').text(hours);
+      container.find('.minutes').text(minutes);
+      container.find('.seconds').text(seconds);
 
-				if (callback && typeof callback === 'function') callback();
+      // Update text labels
+      container.find('.days_text').text(days == 1 ? settings.day : settings.days);
+      container.find('.hours_text').text(hours == 1 ? settings.hour : settings.hours);
+      container.find('.minutes_text').text(minutes == 1 ? settings.minute : settings.minutes);
+      container.find('.seconds_text').text(seconds == 1 ? settings.second : settings.seconds);
+    }
 
-				return;
-			}
+    // Initial call
+    countdown();
 
-			// basic math variables
-			var _second = 1000,
-				_minute = _second * 60,
-				_hour = _minute * 60,
-				_day = _hour * 24;
-
-			// calculate dates
-			var days = Math.floor(difference / _day),
-				hours = Math.floor((difference % _day) / _hour),
-				minutes = Math.floor((difference % _hour) / _minute),
-				seconds = Math.floor((difference % _minute) / _second);
-
-			// based on the date change the refrence wording
-			var text_days = (days === 1) ? settings.day : settings.days,
-				text_hours = (hours === 1) ? settings.hour : settings.hours,
-				text_minutes = (minutes === 1) ? settings.minute : settings.minutes,
-				text_seconds = (seconds === 1) ? settings.second : settings.seconds;
-
-				// fix dates so that it will show two digets
-				days = (String(days).length >= 2) ? days : '0' + days;
-				hours = (String(hours).length >= 2) ? hours : '0' + hours;
-				minutes = (String(minutes).length >= 2) ? minutes : '0' + minutes;
-				seconds = (String(seconds).length >= 2) ? seconds : '0' + seconds;
-
-			// set to DOM
-			container.find('.days').text(days);
-			container.find('.hours').text(hours);
-			container.find('.minutes').text(minutes);
-			container.find('.seconds').text(seconds);
-
-			container.find('.days_text').text(text_days);
-			container.find('.hours_text').text(text_hours);
-			container.find('.minutes_text').text(text_minutes);
-			container.find('.seconds_text').text(text_seconds);
-		}
-
-		// start
-		var interval = setInterval(countdown, 1000);
-	};
-
+    // Update countdown every second
+    var interval = setInterval(countdown, 1000);
+  };
 })(jQuery);
